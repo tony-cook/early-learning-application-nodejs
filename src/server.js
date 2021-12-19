7644e79688f97efbb37efa6c6025ff54e5a86d4f
Tony Cook <tony_cook@outlook.com>
2021-12-20 12:31:39 +1300

    db connection files added for student_project_library_feature

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mysql =require('mysql2');
+const app = express();
+const dotenv = require("dotenv").config({path: __dirname + '/.env'});
+
+const serverTest = require('./utils/connections');
+
+
+app.use();
+
+
+app.listen(4000,serverTest)
