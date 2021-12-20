2989fa79a57f46f38c4e200a10f11a2c89900a1f
Tony Cook <tony_cook@outlook.com>
2021-12-20 21:14:08 +1300

    further bug fixing on teacher_project_library_feature

--- src/controllers/projectsLibrary.js
+++ src/controllers/projectsLibrary.js
@@ -14,12 +14,12 @@ const handleProjectLibrary = (req, res) => {
         const userQuery = req.query
 
 // Defining variables for DB queries: 
- 
         const subscription = userQuery.subscription ? userQuery.subscription : ["free","premium"]
         const activityType = userQuery.activity_type ? userQuery.activity_type : ["animation","game","chatbot","augmentedreality"]
-        const yearLevelMin = userQuery.yearMin ? Math.min(...userQuery.yearMin) : 1
-        const yearLevelMax = userQuery.yearMax ? Math.max(...userQuery.yearMax) : 99
-
+        const yearLevelMin = typeof userQuery.yearMin == "string" ? Number(userQuery.yearMin) : userQuery.yearMin ? Math.min(...userQuery.yearMin) : 1
+        const yearLevelMax = typeof userQuery.yearMax == "string" ? Number(userQuery.yearMax) : userQuery.yearMax ? Math.max(...userQuery.yearMax) : 99
+        console.log(userQuery.yearMax)
+        console.log(Number(userQuery.yearMax))
         const subjectCsc = userQuery.CSC
         const subjectMath = userQuery.MAT
         const subjectSci = userQuery.SCI
